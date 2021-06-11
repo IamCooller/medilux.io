3af5915c87cdace16ccb8cf84bd561930017c7d2
IamCooller <45273146+IamCooller@users.noreply.github.com>
2021-06-11 16:57:49 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ $(document).ready(function() {
         dots: false,
         infinite: true,
         arrows: true,
-        slidesToShow: 6,
+        slidesToShow: 8,
         cssEase: 'linear',
         autoplaySpeed: 0,
         prevArrow: '<svg width="7" class="slick-arrow slick-arrow-prev" height="11" viewBox="0 0 7 11" fill="none" xmlns="https://www.w3.org/2000/svg"><path d="M0.65491 5.19814L5.74273 0.124392C5.90943 -0.0417463 6.17932 -0.041467 6.34573 0.125251C6.51202 0.291948 6.51159 0.561984 6.34487 0.728251L1.55989 5.50002L6.34505 10.2718C6.51174 10.4381 6.51217 10.7079 6.3459 10.8746C6.26248 10.9582 6.15319 11 6.0439 11C5.93489 11 5.82603 10.9585 5.74275 10.8755L0.65491 5.80187C0.574623 5.72199 0.52957 5.61328 0.52957 5.50002C0.52957 5.38675 0.574751 5.27817 0.65491 5.19814Z" fill="#393185"/></svg>',
@@ -62,13 +62,27 @@ $(document).ready(function() {
             {
                 breakpoint: 450,
                 settings: {
-                    slidesToShow: 1
+                    slidesToShow: 2
                 }
             },
 
         ]
     });
-
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+    });
     $('.mobile__button').click(function() {
 
         $('.menu').addClass('open');
@@ -107,6 +121,7 @@ $(document).ready(function() {
         }
 
     });
+
 
 
 
